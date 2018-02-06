c1ca61b0f805b3d9ff75502ea85cc0c78592322c
whapunkt <wolfgangchohlfeld@gmail.com>
2018-02-06 20:29:14 +0100

    whatson raus, speak.dialog rein

--- __init__.py
+++ __init__.py
@@ -24,6 +24,7 @@ NOVA_URL = 'http://st03.dlf.de/dlf/03/128/mp3/stream.mp3'
 ENERGYHH_URL = 'http://185.52.127.131/de/33009/mp3_128.mp3'
 
 URLS =[DLF_URL, DRADIO_URL, NOVA_URL, ENERGYHH_URL]
+NAME = ["DLF","DRADIO","NOVA","ENERGY" ]
 
 POSITION = 0
 
@@ -37,24 +38,6 @@ class RadioChannelSkill(MycroftSkill):
         if AudioService:
             self.audioservice = AudioService(self.emitter)
 
-        '''
-        whatson_dlf_intent = IntentBuilder("WhatsonDlfIntent").\
-                         require("WhatsonKeyword").\
-                         require("DlfKeyword").build()
-        self.register_intent(whatson_dlf_intent, self.handle_whatson_dlf_intent)
-
-        whatson_dradio_intent = IntentBuilder("WhatsonDradioIntent").\
-                                require("WhatsonKeyword").\
-                                require("DradioKeyword").build()
-        self.register_intent(whatson_dradio_intent,
-                             self.handle_whatson_dradio_intent)
-
-        whatson_nova_intent = IntentBuilder("WhatsonNovaIntent").\
-                              require("WhatsonKeyword").\
-                              require("NovaKeyword").build()
-        self.register_intent(whatson_nova_intent,
-                             self.handle_whatson_nova_intent)
-        '''
 
         random_intent = IntentBuilder("RandomIntent"). \
             require("TurnKeyword").require("RadioKeyword").require("OnKeyword").build()
@@ -76,46 +59,9 @@ class RadioChannelSkill(MycroftSkill):
             require("EnergyHHKeyword").require("TurnKeyword").require("RadioKeyword").require("OnKeyword").build()
         self.register_intent(energyhh_intent, self.handle_energyhh_intent)
 
-        '''
-        next_intent = IntentBuilder("NextIntent"). \
-            require("NextKeyword").build()
-        self.register_intent(next_intent, self.handle_next_intent)
-
-        previous_intent = IntentBuilder("PreviousIntent"). \
-            require("PreviousKeyword").build()
-        self.register_intent(previous_intent, self.handle_previous_intent)
-        '''
-
         change_intent = IntentBuilder("ChangeIntent"). \
             require("ChangeKeyword").build()
         self.register_intent(change_intent, self.handle_change_intent)
-
-    '''
-    
-    def handle_whatson_dlf_intent(self, message):
-        r = requests.get('http://www.deutschlandfunk.de')
-        soup = BeautifulSoup(r.text)
-        for el in soup.find_all(id='dlf-player-jetzt-im-radio'):
-            for a_el in el.find_all('a'):
-                self.speak_dialog("currently",
-                                  { "station": "dlf", "title": a_el.string})
-
-    def handle_whatson_dradio_intent(self, message):
-        r = requests.get('http://www.deutschlandfunkkultur.de/')
-        soup = BeautifulSoup(r.text)
-        for el in soup.find_all(id='drk-player-jetzt-im-radio'):
-            for a_el in el.find_all('a'):
-                self.speak_dialog("currently",
-                                  { "station": "dlf culture", "title": a_el.string})
-
-    def handle_whatson_nova_intent(self, message):
-        r = requests.get('https://www.deutschlandfunknova.de/actions/dradio/playlist/onair')
-        j = r.json() 
-        
-        self.speak_dialog("currently",
-                          {"station": "dlf nova", "title": j['show']['title']})
-
-    '''
 
     def handle_random_intent(self, message):
         nr = random.randint(0, 3)
@@ -126,6 +72,7 @@ class RadioChannelSkill(MycroftSkill):
         else:
             self.process = play_mp3(URLS[nr])
             POSITION = nr
+        self.speak_dialog("currently",{"station": NAME[nr]})
 
     def handle_dlf_intent(self, message):
         if self.audioservice:
@@ -163,25 +110,6 @@ class RadioChannelSkill(MycroftSkill):
             self.process = play_mp3(URLS[3])
             POSITION = 3
 
-    '''
-    
-    def handle_next_intent(self, message):
-        global POSITION
-        if self.audioservice:
-            self.audioservice.play(URLS[POSITION+1], message.data['utterance'])
-            POSITION = POSITION + 1
-        else:
-            self.process = play_mp3(URLS[POSITION+1])
-
-    def handle_previous_intent(self, message):
-        global POSITION
-        if POSITION > 0:
-            if self.audioservice:
-                self.audioservice.play(URLS[POSITION-1], message.data['utterance'])
-                POSITION = POSITION -1
-            else:
-                self.process = play_mp3(URLS[POSITION+1])
-    '''
 
     def handle_change_intent(self, message):
         global POSITION
